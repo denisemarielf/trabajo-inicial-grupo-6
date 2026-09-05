048f9b6b39c416e8e3767632b319d1337aa67409
Leandro Alegre <88396808+Ranglerino@users.noreply.github.com>
2026-09-05 15:25:49 -0300

    pj

--- Assets/Scripts/Player/PlayerMovementCC.cs
+++ Assets/Scripts/Player/PlayerMovementCC.cs
@@ -15,6 +15,7 @@ public class PlayerMovementCC : NetworkBehaviour
     private CharacterController controller;
     private Vector2 moveInput;
     private float verticalVelocity;
+    [SerializeField] private Animator animator;
 
     private void Awake()
     {
@@ -27,6 +28,7 @@ public class PlayerMovementCC : NetworkBehaviour
             return;
 
         Move();
+        UpdateAnimator();
         ApplyGravity();
     }
 
@@ -39,10 +41,17 @@ public class PlayerMovementCC : NetworkBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
+        if (!IsOwner) return;
+
         if (context.performed && controller.isGrounded)
         {
             verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+           // if (animator != null)
+             //   animator.SetTrigger("jump");
         }
+
+        
     }
 
     private void Move()
@@ -53,6 +62,11 @@ public class PlayerMovementCC : NetworkBehaviour
         controller.Move(movement * moveSpeed * Time.deltaTime);
     }
 
+    private void UpdateAnimator()
+    {
+        if (animator == null) return;
+        animator.SetFloat("speed", moveInput.magnitude);
+    }
     private void ApplyGravity()
     {
         if (controller.isGrounded && verticalVelocity < 0)
